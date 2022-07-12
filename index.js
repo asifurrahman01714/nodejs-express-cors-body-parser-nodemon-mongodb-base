c2e127a2fc4dfb5d3289cd83cf563337e6fcc56b
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-07-12 22:48:09 +0600

    Added env file + mongodb+ nodejs code

--- index.js
+++ index.js
@@ -4,12 +4,75 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+console.log(process.env.DB_PASS,process.env.DB_USER)
 
+// firebase admin set up for verify idToken
+const admin = require("firebase-admin");
+const serviceAccount = require("./burj-al-arab-bafe3-firebase-adminsdk-q5wtu-5b0a97a05d.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.use(cors())
 // parse application/json
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })); // for file
+
+// MongoDB connection
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoDbUserPassword = process.env.DB_PASS;
+const mongoDbUser = process.env.DB_USER;
+const uri = `mongodb+srv://${mongoDbUser}:${mongoDbUserPassword}@atlascluster.eb7mhhm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("burjAlArabHotel").collection("bookings");
+  console.log('database connected');
+  // Post data
+  app.post("/addBooking", (req, res)=>{
+    const booking = req.body;
+    collection.insertOne(booking);
+    console.log(booking);
+    res.send(booking);
+  });
+
+  // Get Data
+  app.get("/bookings",(req, res)=>{
+    console.log(req.query.email);
+    console.log(req.headers.authorization);
+    const bearer = req.headers.authorization;
+    if (bearer && bearer.startsWith('Bearer ')) {
+        const idToken = bearer.split(' ')[1];
+        console.log({idToken});
+        // idToken comes from the client app
+        admin.auth()
+        .verifyIdToken(idToken)
+        .then((decodedToken) => {
+        const uid = decodedToken.uid;
+        const decodedEmail = decodedToken.email;
+        const queryEmail = req.query.email;
+        console.log({uid});
+        if (decodedEmail == queryEmail) {
+            collection.find({email : queryEmail})
+            .toArray((err, documents)=>{
+                res.send(documents)
+            })
+        }else{
+            res.status(401).send("Un- authorized User")
+        }
+        
+        })
+        .catch((error) => {
+        // Handle error
+        });
+    }else{
+        res.status(401).send("Un- authorized User")
+    }
+    
+  })
+
+});
+
 
 app.get("/", (req,res)=>{
     console.log("Requesting anyone");
@@ -17,7 +80,9 @@ app.get("/", (req,res)=>{
 })
 
 
-app.listen(3000, ()=> console.log("Yay! Listening to port 3000"))
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("Server is running on port 5000");
+})
 
 
 
